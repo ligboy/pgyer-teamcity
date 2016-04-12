619a22cb400d82581c1e24d0f7bd6f94e702cf23
ligboy <ligboy.liu@heysroad.com>
2016-04-12 16:46:51 +0800

    Fixed filepath null pointer exception

--- pgyer-agent/src/main/java/org/ligboy/teamcity/pgyer/agent/PgyerAgentBuildRunner.java
+++ pgyer-agent/src/main/java/org/ligboy/teamcity/pgyer/agent/PgyerAgentBuildRunner.java
@@ -3,6 +3,7 @@ package org.ligboy.teamcity.pgyer.agent;
 import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.*;
 import jetbrains.buildServer.util.AntPatternFileFinder;
+import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.ligboy.teamcity.pgyer.PgyerConstants;
 
@@ -25,18 +26,20 @@ public class PgyerAgentBuildRunner implements AgentBuildRunner {
         String installPassword = runnerParameters.get(PgyerConstants.PARAM_INSTALL_PASSWORD);
         String uploadInstructs = runnerParameters.get(PgyerConstants.PARAM_UPDATE_DESCRIPTION);
         boolean isPublishToPublic = Boolean.valueOf(runnerParameters.get(PgyerConstants.PARAM_PUBLISH_TO_PUBLIC));
-        AntPatternFileFinder patternFileFinder = new AntPatternFileFinder(new String[] {filePath}, null, false);
-        File file = null;
-        try {
-            File[] files = patternFileFinder.findFiles(runningBuild.getCheckoutDirectory());
-            if (files != null && files.length > 0) {
-                file = files[0];
+        if (!StringUtil.isEmptyOrSpaces(filePath)) {
+            AntPatternFileFinder patternFileFinder = new AntPatternFileFinder(new String[] {filePath}, new String[]{}, false);
+            File file = null;
+            try {
+                File[] files = patternFileFinder.findFiles(runningBuild.getCheckoutDirectory());
+                if (files != null && files.length > 0) {
+                    file = files[0];
+                }
+            } catch (IOException e) {
+                throw new RunBuildException(e);
             }
-        } catch (IOException e) {
-            throw new RunBuildException(e);
-        }
-        if (file != null && file.exists()) {
-            return new PgyerBuildProcessAdapter(context, apiKey, userKey, file, uploadInstructs, installPassword, isPublishToPublic);
+            if (file != null && file.exists()) {
+                return new PgyerBuildProcessAdapter(context, apiKey, userKey, file, uploadInstructs, installPassword, isPublishToPublic);
+            }
         }
         throw new RunBuildException("\"" + filePath + "\" is not exists.");
     }
